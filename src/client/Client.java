d927c2505ad8d3de6d66ee612b1b30b7c8700b3f
smirnova <dara071@yandex.ru>
2021-11-07 23:11:54 +0300

    output stream of a serialized object

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,28 +1,46 @@
 package client;
+// Здесь принимаем объект просто для наглядности. Теперь клиента запускаем через графический вариант
+// Например через проект E:\ITcourse\professional_2021\java_educational_projects\folder_chats_1313\graphical_03112021_laba_10.1\GUI_chat_1313
 
-import java.io.DataInputStream;
+import java.io.DataInputStream; // заменен на ObjectInputStream
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
 public static void main(String[] args) {
     try {
         Scanner scanner = new Scanner(System.in);
-        Socket socket = new Socket("127.0.0.1", 8179); //8178
+        Socket socket = new Socket("127.0.0.1", 8179); //8178 // 127.0.0.1 // 5.181.108.28
         System.out.println("Успешно подключен");
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        DataInputStream in = new DataInputStream(socket.getInputStream());
+        //DataInputStream in = new DataInputStream(socket.getInputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     while (true) {
-                        String response = in.readUTF();
-                        System.out.println(response);
+                        //String response = in.readUTF();
+                        //System.out.println(response);
+                        ArrayList<String> usersName = new ArrayList(); //Создадим здесь идентичную коллекцию как и на стороне сервера
+                        String response; // Присвоим значение, когда выясним, что присланный объет именно строка
+                        Object object = ois.readObject(); // тут снова требуется try/catch, т.к. исключения могут быть не только IOException, заменяем его на Exception
+                        //System.out.println(object.getClass()); // метод getClass() покажет в консоли тип объекта (строка, массив, еще что-то) // class java.lang.String
+
+                        if(object.getClass().equals(usersName.getClass())) { // если пришел тип объекта ArrayList, то
+                            usersName = (ArrayList<String>) object; // этот объект надо преобразовать в коллекцию с помощью кастования - преобразования типов
+                            System.out.println("Пользователи онлайн: "+usersName); // просто для наглядности
+                        }
+                        else {
+                            response = object.toString(); // Преобразуем в строку
+                            System.out.println(response);
+                        }
                     }
-                } catch (IOException e) {
+                } catch (Exception e) {// IOException -> Exception
                     e.printStackTrace();
                     System.out.println("нет подключения.");
                 }
@@ -36,6 +54,7 @@ public static void main(String[] args) {
         }
     } catch (IOException e) {
         e.printStackTrace();
+        System.out.println("Подключиться по указанныем ip и/или порту не удалось.");
     }
 }
 }
--- src/server/User.java
+++ src/server/User.java
@@ -1,26 +1,31 @@
 package server;
 // Пользователь идентифицируется по соккету, не используем uuid
+// DataOutputStream out -> OojectOutputStream oos
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class User {
 private Socket socket;
 private String userName;
-private DataOutputStream out;
-private DataInputStream in;
+//private DataOutputStream out; // уже не нужен
+private DataInputStream in; // не удаляем, т.к. используется в файле Server.java
+private ObjectOutputStream oos;
 
 public User(Socket socket) throws IOException {
 	this.socket = socket;
-	this.in = new DataInputStream(socket.getInputStream());
-	this.out = new DataOutputStream(socket.getOutputStream());
+	this.in = new DataInputStream(socket.getInputStream()); // Используется в серверной части для получения имени и сообщений от клиента
+	//this.out = new DataOutputStream(socket.getOutputStream());
+	this.oos = new ObjectOutputStream(socket.getOutputStream()); // чтобы с сервера отправлять объект по сети
 }
 public String getUserName() {return userName;}
 public void setUserName(String userName) {this.userName = userName;}
 
-public DataOutputStream getOut() {return out;}
+//public DataOutputStream getOut() {return out;}
 public DataInputStream getIn() {return in;}
+public ObjectOutputStream getOos() {return oos;} // создаем геттер для использования этого свойства
 
 public Socket getSocket() {
 	return socket;
